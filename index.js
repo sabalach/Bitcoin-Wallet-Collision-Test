f25b754793e3a9cfd77cb115c8e37a94a2e4f23a
prosabal <prohero38@gmail.com>
2019-10-13 19:35:35 +0545

    Code refactored

--- index.js
+++ index.js
@@ -7,41 +7,47 @@ const port = 3000;
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/bitwallet');
 
-
-
 const walletSchema = new mongoose.Schema({
     publicK: String,
     privateK: String,
     bl: Number
 });
+
 const Wallet = mongoose.model('Wallet', walletSchema);
 
 const interval = setInterval(async () => {
     const keyPair = bitcoin.ECPair.makeRandom();
     
     const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey })
-    // console.log("address " + address);
     
-    const wif = keyPair.toWIF();
-    // console.log("private key WIF " + wif)
     const pkey = keyPair.toWIF();
     
     const url = `https://blockchain.info/q/addressbalance/${address}`;
-    const response = await axios.get(url);
-    if(response.data != 0){
-        const acc = new Wallet({ publicK: address, privateK: pkey, bl: response.data});
-        acc.save(function(err){
-            if (err) return console.error(err);
-        });
-        console.log('has balance');
-    } else {
-        console.log('no balance');
+    try {
+        const response = await axios.get(url);
+        if(response.data !== 0){
+            const acc = new Wallet({ publicK: address, privateK: pkey, bl: response.data});
+            console.log({address, pkey});
+            acc.save(function(err){
+                if (err) return console.error(err);
+            });
+            console.log('has balance');
+        } else {
+            console.clear();
+            console.log({address, bl: response.data});
+        }
+    } catch (err){
+        console.log({err})
     }
-}, 2000);
+}, 450);
 
 app.get('/', async(req, res) => {
-    const wallets = await Wallet.find().exec();
-    res.send(wallets)
+    res.send('Hlo Guys');
+});
+
+app.get('/w', async(req, res) => {
+    const wallets = await Wallet.find();
+    res.send(wallets);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
